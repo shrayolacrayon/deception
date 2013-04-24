12aa9f792c604f7526144a660437148f95362d4e
sr527 <sr527@cornell.edu>
2013-04-24 19:24:53 -0400

    things

--- knn.py
+++ knn.py
@@ -1,1 +1,14 @@
 #knn.py
+from future import division
+import math
+
+def calc_distance(vector1, vector2):
+  s = 0
+  for i,v in enumerate(vector1):
+    s += ((v - vector2[i]) ** 2)
+  return math.sqrt(s)
+
+def find_nearest(test, train_set):
+  pass
+
+calc_distance([1,0,0,0,0], [0,0,0,0,0])
