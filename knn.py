f5ca18f258ca1ef28cb5101590aa6c5928d42c79
sr527 <sr527@cornell.edu>
2013-04-25 15:15:53 -0400

    knn

--- knn.py
+++ knn.py
@@ -1,6 +1,7 @@
 #knn.py
-from future import division
+from __future__ import division
 import math
+import sys
 
 def calc_distance(vector1, vector2):
   s = 0
@@ -8,7 +9,23 @@ def calc_distance(vector1, vector2):
     s += ((v - vector2[i]) ** 2)
   return math.sqrt(s)
 
-def find_nearest(test, train_set):
-  pass
+def find_nearest(test, train_set, k):
+  min_dist = [float(sys.maxint)] * k
+  min_ind = [0] * k
+  for i,t in enumerate(train_set):
+    #index of maximum value
+    index = min_dist.index(max(min_dist))
+    val = max(min_dist)
+    dist = calc_distance(test,t)
+    if val > dist:
+      min_dist[index] = dist
+      min_ind[index] = i
+  return zip(min_ind, min_dist)
+  
 
-calc_distance([1,0,0,0,0], [0,0,0,0,0])
+
+
+
+
+
+
